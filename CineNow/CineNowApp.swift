9c5ef921ca23cbbc38a8fc85dd0687e70385d994
NJ Development <nj.development83@gmail.com>
2026-02-04 00:15:07 -0300

    Initial Commit

---
+++ CineNow/CineNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineNowApp.swift
+//  CineNow
+//
+//  Created by NJ Development on 04/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CineNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineNowTests/CineNowTests.swift
@@ -0,0 +1,16 @@
+//
+//  CineNowTests.swift
+//  CineNowTests
+//
+//  Created by NJ Development on 04/02/26.
+//
+
+import Testing
+
+struct CineNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
